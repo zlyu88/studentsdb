bcd773515f08fc3c10735fd0e1b2e8c98488cb14
zlyu88 <zlyu88@gmail.com>
2016-06-13 19:00:37 +0300

    templates and tags

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns('',
+    # Students URLs
+    url(r'^$', 'students.views.students_list', name='home'),
+    url(r'^students/add/$', 'students.views.students_add', name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit',
+        name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete',
+        name='students_delete'),
+
+    # Groups URLs
+    url(r'^groups/$', 'students.views.groups_list', name='groups'),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
